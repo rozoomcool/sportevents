03238a192ba8622f4a2b771037d8fe8c4fe4e3f2
rozoomcool <rozoom.um@gmail.com>
2024-11-22 16:02:15 +0300

    .

--- backend/src/main/kotlin/com/govzcode/sportevents/service/CustomUserDetailsService.kt
+++ backend/src/main/kotlin/com/govzcode/sportevents/service/CustomUserDetailsService.kt
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
+import kotlin.jvm.optionals.getOrNull
 
 @Service
 class CustomUserDetailsService(
