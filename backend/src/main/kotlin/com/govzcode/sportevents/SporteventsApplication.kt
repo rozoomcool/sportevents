830013fc7d5014890b24c68d83f1fbe70f9dc54b
rozoomcool <rozoom.um@gmail.com>
2024-11-22 14:49:34 +0300

    init back

---
+++ backend/src/main/kotlin/com/govzcode/sportevents/SporteventsApplication.kt
@@ -0,0 +1,11 @@
+package com.govzcode.sportevents
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SporteventsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SporteventsApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/govzcode/sportevents/SporteventsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.govzcode.sportevents
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SporteventsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
